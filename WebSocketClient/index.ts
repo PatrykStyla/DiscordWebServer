09792e6eb9b87e9d1aff5b4766626a8f9a80cc58
Patryk Styla <43710236+ws295203@users.noreply.github.com>
2021-05-28 00:36:59 +0000

    websocket client to communicate with the bot

---
+++ WebSocketClient/index.ts
@@ -0,0 +1,38 @@
+import ws from "ws";
+// import { ctx } from "..";
+
+let WebSocket: ws
+
+function connect() {
+	WebSocket = new ws("wss://patrykstyla.com:9001")
+	WebSocket.onopen = () => {
+		// console.log(ctx.green('Bot socket is ready'))
+	};
+	// Bot will not receive messages
+	WebSocket.onmessage =((event) => {
+	})
+
+	WebSocket.onerror = (event) => {
+		// console.log(event)         
+	}
+
+	WebSocket.onclose = (event) => {
+		setTimeout(() => {
+			connect()	
+		}, 1000);
+	}
+}
+
+setInterval(() => {
+	if (WebSocket.readyState === WebSocket.OPEN) {
+		WebSocket.send("")
+	}
+}, 25000);
+
+connect()
+// TODO: send wraped in json 
+function SendMessageToWebSocket(Encoded: Uint8Array, guild_id: string = "") {
+	WebSocket.send(Encoded)
+}
+
+export { WebSocket, SendMessageToWebSocket }
